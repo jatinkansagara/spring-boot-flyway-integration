dc86429cfc63606836f4e79e71a30d9901415b44
JK <jatinkansagara@gmail.com>
2020-03-02 09:42:24 +0530

    Initial Commit

---
+++ src/main/resources/db/migration/V1_2__Insert_Test.sql
@@ -0,0 +1,1 @@
+insert into test(id) values(10);
---
+++ src/test/java/com/flyway/FlywayProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
